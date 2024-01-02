a74b43b35f0b0cd580212349bcc4e1752e0d838f
James Zhou <jam9es@gmail.com>
2024-01-01 17:05:04 -0800

    Refactor handleAddProject function
    Instead of adding onto DOM using JS, the add project section is written
    in HTML and use class names to toggle DOM on and off.

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,32 +1,26 @@
 // DOM manipulation - add project
 export const addProject = (function() {
     const addProjectBtn = document.querySelector('.addProjectBtn');
+    const addProjectField = document.querySelector('#display-add-project');
+    const projectCancelBtn = document.querySelector('.project-cancel-btn');
+
+    // show add project
     addProjectBtn.addEventListener('click', () => handleAddProject());
     function handleAddProject() {
-        // remove add project button
+        // hide button
         addProjectBtn.classList.add('add-project-active');
 
-        const addProjectField = document.querySelector('#display-add-project');
-        const addProjectContainer = document.createElement('div');
-        const addProjectInput = document.createElement('input');
-        const projectBtnField = document.createElement('div');
-        const projectAddBtn = document.createElement('button');
-        const projectCancelBtn = document.createElement('button');
+        // show input field
+        addProjectField.classList.remove('hide-input');
+    };
 
-        addProjectContainer.classList.add('add-project-container');
-        addProjectInput.classList.add('add-project-input');
-        addProjectInput.type = 'text';
-        projectBtnField.classList.add('project-btn-field');
-        projectAddBtn.classList.add('project-add-btn');
-        projectCancelBtn.classList.add('project-cancel-btn');
+    // cancel add project action
+    projectCancelBtn.addEventListener('click', () => returnAddProjectBtn());
+    function returnAddProjectBtn() {
+        // hide input
+        addProjectField.classList.add('hide-input');
 
-        projectAddBtn.textContent = 'Add';
-        projectCancelBtn.textContent = 'Cancel';
-
-        projectBtnField.appendChild(projectAddBtn);
-        projectBtnField.appendChild(projectCancelBtn);
-        addProjectContainer.appendChild(addProjectInput);
-        addProjectContainer.appendChild(projectBtnField);
-        addProjectField.appendChild(addProjectContainer);
+        // show button
+        addProjectBtn.classList.remove('add-project-active');
     };
 })();
